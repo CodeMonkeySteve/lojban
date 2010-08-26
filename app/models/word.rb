1efd2e7383d5b185312e61e4c52f1730c16fadba
Steve Sloan <steve@finagle.org>
2010-08-25 19:42:58 -0700

    Added uniqueness constraint on word name, indexes for searching.

--- app/models/word.rb
+++ app/models/word.rb
@@ -2,13 +2,15 @@ class Word
   include Mongoid::Document
   extend ActiveSupport::Memoizable
 
-  field :name, :type => String, :required => true
+  field :name, :type => String, :required => true, :unique => true
   field :parts, :type => Array  # of String
   embeds_many :denotations
   references_many :related, :class_name => 'Word', :stored_as => :array
   references_many :synonyms, :class_name => 'Word', :stored_as => :array
 
   index :name, :unique => true
+  index 'denotations.lang'
+  index 'denotations.translations'
 
   def local
     denotations.where( :lang => I18n.locale.to_s ).first
